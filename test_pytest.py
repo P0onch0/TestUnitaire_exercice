74406fb895eabfd8ffcb6df44cf926ec0c9d721c
P0onch0 <ewan.legaut@gmail.com>
2024-10-29 15:31:41 +0100

    2eme Test Unitaire

--- test_pytest.py
+++ test_pytest.py
@@ -6,3 +6,9 @@ def test_addition(sample_data):
     # Utilisez sample_data comme paramètre de test
     result = addition(sample_data['a'], sample_data['b'])
     assert result == 8
+
+def op(a):
+    return a + 5
+ 
+def test_op():
+    assert op(2) == 7
